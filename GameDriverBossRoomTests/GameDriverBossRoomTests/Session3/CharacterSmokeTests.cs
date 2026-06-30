b1e60b269410c716551bf18c5307eda154447f15
Andrew Fray <andrew.fray@gmail.com>
2026-06-30 19:04:57 +0100

    character smoke tests

---
+++ GameDriverBossRoomTests/GameDriverBossRoomTests/Session3/CharacterSmokeTests.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using gdio.common.objects;
+
+namespace GameDriverBossRoomTests.Session3;
+
+class CharactersEnumerator : IEnumerable<int>
+{
+    public IEnumerator<int> GetEnumerator() => Enumerable.Range(0, 8).GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+[TestFixtureSource(typeof(CharactersEnumerator))]
+public class CharacterSmokeTests : GameDriverTest
+{
+    private LogTracker logs = new();
+    private int charIndex;
+
+    public CharacterSmokeTests(int index)
+    {
+        charIndex = index;
+    }
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        logs.OnOneTimeSetUp(api);
+        
+        api.WaitForObjectValue("/*[@name='LoadingScreen']/fn:component('UnityEngine.CanvasGroup')", "alpha", 0f);
+        
+        api.Wait(500);
+
+        api.CallMethod("/*/fn:component('Unity.BossRoom.Gameplay.GameState.ClientMainMenuState')",
+            "OnDirectIPClicked");
+        
+        api.ClickObject(MouseButtons.LEFT, "//Test_IpSessionHostBtn", 1);
+        api.WaitForEmptyInput();
+        api.WaitForObject("/*[fn:component(\"Unity.BossRoom.Gameplay.GameState.ClientCharSelectState\")]");
+                
+        api.WaitForObjectValue("/*[@name='LoadingScreen']/fn:component('UnityEngine.CanvasGroup')", "alpha", 0f);
+        
+        // hide the network simulator, it's annoying
+        api.CallMethod(
+            "/*[contains(.,fn:type('Unity.Multiplayer.Tools.NetworkSimulator.Runtime.NetworkSimulator'))][0]//fn:component('Unity.BossRoom.Utils.NetworkSimulatorUIMediator')",
+            "Hide");
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+        logs.OnSetUp();
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+        logs.OnOneTimeTearDown(api);
+    }
+
+    [Test, Order(010)]
+    public void T010_GivenLobby_SelectCharacter()
+    {
+        api.ClickObject(MouseButtons.LEFT, 
+            $"//Test_CharSelectSeat[{charIndex}]//*[contains(.,fn:type('UnityEngine.UI.Button'))]", 1);
+        api.WaitForEmptyInput();
+        
+        api.Wait(1000);
+        
+        logs.AssertThatClearOfErrors();
+    }
+
+    [Test, Order(020)]
+    public void T020_GivenSelectedCharInLobby_WhenStartingGame_EntersGameplayWithNoErrors()
+    {
+        api.CallMethod("/*/fn:component('Unity.BossRoom.Gameplay.GameState.ClientCharSelectState')", "OnPlayerClickedReady");
+        
+        while (api.GetSceneName() != "BossRoom")
+        {
+            api.Wait(500);
+        }
+        
+        api.WaitForObjectValue("/*[@name='LoadingScreen']/fn:component('UnityEngine.CanvasGroup')", "alpha", 0f);
+        
+        // hide the cheats panel
+        api.SetObjectFieldValue("//*[@name='CheatsPopupPanel']", "active", false);
+        // also the how to play. these are both how the close buttons work directly.
+        api.SetObjectFieldValue("//*[@name='HowToPlayPopupPanel']", "active", false);
+        
+        api.Wait(1000);
+        
+        logs.AssertThatClearOfErrors();
+    }
+
+    [Test, Order(030)]
+    public void T030_GivenIdleGame_WhenReturningToMenu_ReturnsWithNoErrors()
+    {
+        api.CallMethod(
+            "/*[fn:component('Unity.BossRoom.Gameplay.UI.UISettingsCanvas')][0]//fn:component('Unity.BossRoom.Gameplay.UI.UIQuitPanel')",
+            "Quit");
+        
+        while (api.GetSceneName() == "BossRoom")
+        {
+            api.Wait(500);
+        }
+        
+        api.WaitForObjectValue("/*[@name='LoadingScreen']/fn:component('UnityEngine.CanvasGroup')", "alpha", 0f);
+        
+        api.Wait(1000);
+        
+        logs.AssertThatClearOfErrors();
+    }
+}
---
+++ GameDriverBossRoomTests/GameDriverBossRoomTests/Session3/LogTracker.cs
@@ -0,0 +1,39 @@
+﻿using gdio.common.objects;
+using gdio.unity_api.v2;
+
+namespace GameDriverBossRoomTests.Session3;
+
+public class LogTracker
+{
+    private bool isConsoleError;
+    private readonly List<string> outputs = new();
+
+    public void OnOneTimeSetUp(ApiClient api)
+    {
+        api.UnityLoggedMessage += OnUnityLog;
+    }
+
+    public void OnSetUp()
+    {
+        isConsoleError = false;
+        outputs.Clear();
+    }
+
+    public void AssertThatClearOfErrors()
+    {
+        Console.WriteLine(string.Join("\n", outputs));
+        Assert.That(isConsoleError, Is.False);
+    }
+
+    public void OnOneTimeTearDown(ApiClient api)
+    {
+        api.UnityLoggedMessage -= OnUnityLog;
+    }
+    
+    private void OnUnityLog(object? sender, UnityLogEventEventArgs args)
+    {
+        isConsoleError |= args.type is LogType.Error or LogType.Exception or LogType.Assert;
+        var msg = $"[{args.type}] {args.condition}";
+        outputs.Add(msg);
+    }
+}
