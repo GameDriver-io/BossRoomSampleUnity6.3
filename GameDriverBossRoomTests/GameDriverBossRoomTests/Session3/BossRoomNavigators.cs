29d6e8af078f443c43d7d4a70cd3e14f7144784a
Andrew Fray <andrew.fray@gmail.com>
2026-07-02 12:09:23 +0100

    multiplayer regression test for movement, both ways

---
+++ GameDriverBossRoomTests/GameDriverBossRoomTests/Session3/BossRoomNavigators.cs
@@ -0,0 +1,142 @@
+﻿using System.Diagnostics;
+using gdio.common.objects;
+using gdio.unity_api.v2;
+
+namespace GameDriverBossRoomTests.Session3;
+
+public static class BossRoomNavigators
+{
+    public static void WaitUntilBooted(ApiClient api) => WaitUntilLeftScene(api, "Startup");
+
+    public static void WaitUntilLeftScene(ApiClient api, string sceneToLeave)
+    {
+        Assert.That(api, Is.Not.Null);
+        Assert.That(sceneToLeave, Is.Not.Null);
+        Assert.That(sceneToLeave, Is.Not.Empty);
+
+        while (api.GetSceneName() == sceneToLeave)
+        {
+            api.Wait(200);
+        }
+        
+        // and for loading screen to be gone
+        api.WaitForObjectValue("/Test_LoadingScreen/fn:component('UnityEngine.CanvasGroup')", 
+            "alpha", 
+            0f);
+    }
+    
+    public static void WaitUntilLoadedScene(ApiClient api, string sceneName)
+    {
+        Assert.That(api, Is.Not.Null);
+        Assert.That(sceneName, Is.Not.Null);
+        Assert.That(sceneName, Is.Not.Empty);
+
+        while (api.GetSceneName() != sceneName)
+        {
+            api.Wait(200);
+        }
+        // and for loading screen to be gone
+        api.WaitForObjectValue("/Test_LoadingScreen/fn:component('UnityEngine.CanvasGroup')", 
+            "alpha", 
+            0f);
+    }
+    
+    public static void HostLocalIp(ApiClient api)
+    {
+        Assert.That(api, Is.Not.Null);
+
+        api.ClickObject(MouseButtons.LEFT, "//Test_MainMenuLocalHostBtn", 1);
+        api.WaitForEmptyInput();
+        api.ClickObject(MouseButtons.LEFT, "//Test_IpSessionHostBtn", 1);
+        api.WaitForEmptyInput();
+        
+        WaitUntilLoadedScene(api, "CharSelect");
+
+        // Hide the network simulator overlay so it doesn't interfere with clicks
+        api.CallMethod(
+            "/*[contains(.,fn:type('Unity.Multiplayer.Tools.NetworkSimulator.Runtime.NetworkSimulator'))][0]//fn:component('Unity.BossRoom.Utils.NetworkSimulatorUIMediator')",
+            "Hide");
+    }
+
+    public static void JoinLocalIp(ApiClient api)
+    {
+        Assert.That(api, Is.Not.Null);
+        
+        api.ClickObject(MouseButtons.LEFT, "//Test_MainMenuLocalHostBtn", 1);
+        api.WaitForEmptyInput();
+        api.ClickObject(MouseButtons.LEFT,
+            "//*[@name='IPPopup']/*[@name='Tab Buttons']/*[@name='JoinButton']", 1);
+        api.WaitForEmptyInput();
+        api.ClickObject(MouseButtons.LEFT,
+            "//*[@name='IPPopup']/*[@name='Tabs']/*[@name='IPJoiningUI']/*[@name='Join IP Connection Button']", 1);
+        api.WaitForEmptyInput();
+        
+        WaitUntilLoadedScene(api, "CharSelect");
+        
+        // Hide the network simulator overlay so it doesn't interfere with clicks
+        api.CallMethod(
+            "/*[contains(.,fn:type('Unity.Multiplayer.Tools.NetworkSimulator.Runtime.NetworkSimulator'))][0]//fn:component('Unity.BossRoom.Utils.NetworkSimulatorUIMediator')",
+            "Hide");
+    }
+
+    public static void SelectCharAndReady(ApiClient api, int charIndex)
+    {
+        Assert.That(api, Is.Not.Null);
+        Assert.That(charIndex, Is.GreaterThanOrEqualTo(0));
+        
+        api.ClickObject(MouseButtons.LEFT,
+            $"//Test_CharSelectSeat[{charIndex}]//*[contains(.,fn:type('UnityEngine.UI.Button'))]", 
+            1);
+        api.WaitForEmptyInput();
+        api.Wait(200);
+        
+        api.CallMethod("/*/fn:component('Unity.BossRoom.Gameplay.GameState.ClientCharSelectState')", 
+            "OnPlayerClickedReady");
+    }
+
+    public static void WaitForIdleGameplay(ApiClient api)
+    {
+        Assert.That(api, Is.Not.Null);
+        
+        WaitUntilLoadedScene(api, "BossRoom");
+        api.Wait(500);
+        // hide the cheats panel
+        api.CallMethod("//*[@name='CheatsPopupPanel']", "SetActive", [false]);
+
+        // also the how to play. these are both how the close buttons work directly.
+        api.CallMethod("//*[@name='HowToPlayPopupPanel']", "SetActive", [false]);
+
+        api.Wait(200);
+    }
+
+    public static readonly string LocalPlayerHPath =
+        "//Player/fn:component('Unity.Netcode.NetworkObject')[@IsOwner='true']/@gameObject";
+    public static readonly string RemotePlayerHPath =
+        "//Player/fn:component('Unity.Netcode.NetworkObject')[@IsOwner='false']/@gameObject";
+
+    public static void MoveLocalPlayer(ApiClient api)
+    {
+        Assert.That(api, Is.Not.Null);
+        
+        var playerStartScreenPosition = 
+            api.GetObjectPosition(LocalPlayerHPath, CoordinateConversion.WorldToScreenPoint);
+        
+        var screenDestination =new Vector2(playerStartScreenPosition.x, playerStartScreenPosition.y+100); 
+        api.Click(screenDestination, MouseButtons.LEFT,10);
+        api.WaitForEmptyInput();
+        
+        api.Wait(1000);
+    }
+
+    public static Vector3 GetLocalPlayerWorldPosition(ApiClient api) => api.GetObjectPosition(LocalPlayerHPath);
+    public static Vector3 GetRemotePlayerWorldPosition(ApiClient api) => api.GetObjectPosition(RemotePlayerHPath);
+
+    public static void WaitForAllPlayersJoined(ApiClient server, int expectedPlayerCount)
+    {
+        Assert.That(server, Is.Not.Null);
+        Assert.That(expectedPlayerCount, Is.GreaterThanOrEqualTo(1));
+        
+        // todo: can I count /Player from a hpath? 
+        server.WaitForObject($"/Player[{expectedPlayerCount - 1}]");
+    }
+}
--- GameDriverBossRoomTests/GameDriverBossRoomTests/Session3/MultiplayerTests.cs
+++ GameDriverBossRoomTests/GameDriverBossRoomTests/Session3/MultiplayerTests.cs
@@ -9,162 +9,58 @@ public class MultiplayerTests : GameDriverTest
 {
     private Process standaloneProcess;
     private ApiClient apiP2;
-    private bool isConsoleError;
-    private readonly List<string> outputs = [];
 
     [OneTimeSetUp]
-    public void ConnectP2AndInitLogging()
+    public void TwoPlayersToGameplay()
     {
         // Launch the standalone build and connect P2 to it
         standaloneProcess = StandaloneProcess.Launch();
         apiP2 = new ApiClient();
         apiP2.Connect("localhost", 19735);
-        api.UnityLoggedMessage += OnUnityLog;
+        
+        BossRoomNavigators.WaitUntilBooted(api);
+        BossRoomNavigators.HostLocalIp(api);
+
+        BossRoomNavigators.WaitUntilBooted(apiP2);
+        BossRoomNavigators.JoinLocalIp(apiP2);
+        
+        BossRoomNavigators.SelectCharAndReady(apiP2, 1);
+        BossRoomNavigators.SelectCharAndReady(api, 0);
+
+        BossRoomNavigators.WaitForIdleGameplay(api);
+        BossRoomNavigators.WaitForIdleGameplay(apiP2);
+        BossRoomNavigators.WaitForAllPlayersJoined(api, expectedPlayerCount:2);
     }
 
     [OneTimeTearDown]
     public void DisconnectP2AndCleanupLogging()
     {
-        api.UnityLoggedMessage -= OnUnityLog;
-        apiP2?.Disconnect();
+        apiP2.Disconnect();
         // Kill the standalone build process
         standaloneProcess?.Kill();
         standaloneProcess?.Dispose();
     }
 
-    [SetUp]
-    public void SetUp()
+    public enum Protagonist
     {
-        isConsoleError = false;
-        outputs.Clear();
-        // Wait for both clients to finish booting past the Startup scene
-        while (api.GetSceneName() == "Startup")
-            api.Wait(100);
-        while (apiP2.GetSceneName() == "Startup")
-            apiP2.Wait(100);
-    }
+        Server,
+        Client,
+    } 
 
-    [Test, Order(010)]
-    public void T010_GivenBothOnMainMenu_WhenHostCreatesAndClientJoins_HostSelectionVisibleOnClient()
+    [TestCase(Protagonist.Client), TestCase(Protagonist.Server)]
+    public void GivenBothInGame_WhenProtagonistMoves_MovementReflectedOnOther(Protagonist protagonist)
     {
-        // P1 (host/editor): open the IP popup and host a session
-        api.ClickObject(MouseButtons.LEFT, "//Test_MainMenuLocalHostBtn", 1);
-        api.WaitForEmptyInput();
-        api.ClickObject(MouseButtons.LEFT, "//Test_IpSessionHostBtn", 1);
-        api.WaitForEmptyInput();
-        // Wait until P1 reaches the character select screen
-        api.WaitForObject("/*[fn:component('Unity.BossRoom.Gameplay.GameState.ClientCharSelectState')]");
-
-        // Hide the network simulator overlay so it doesn't interfere with clicks
-        api.CallMethod(
-            "/*[contains(.,fn:type('Unity.Multiplayer.Tools.NetworkSimulator.Runtime.NetworkSimulator'))][0]//fn:component('Unity.BossRoom.Utils.NetworkSimulatorUIMediator')",
-            "Hide");
-
+        var protagApi = protagonist == Protagonist.Server ? api : apiP2;
+        var viewerApi = protagonist == Protagonist.Server ? apiP2 : api;
         
-        api.Wait(500);
-
-        // P2 (client/standalone): open the IP popup, switch to join tab, and connect
-        apiP2.ClickObject(MouseButtons.LEFT, "//Test_MainMenuLocalHostBtn", 1);
-        apiP2.WaitForEmptyInput();
-        apiP2.ClickObject(MouseButtons.LEFT,
-            "//*[@name='IPPopup']/*[@name='Tab Buttons']/*[@name='JoinButton']", 1);
-        apiP2.WaitForEmptyInput();
-        apiP2.ClickObject(MouseButtons.LEFT,
-            "//*[@name='IPPopup']/*[@name='Tabs']/*[@name='IPJoiningUI']/*[@name='Join IP Connection Button']", 1);
-        apiP2.WaitForEmptyInput();
-        // Wait until P2 fully transitions into the character select scene
-        while (apiP2.GetSceneName() != "CharSelect")
-            apiP2.Wait(500);
-        // Hide the network simulator on P2 as well
-        apiP2.CallMethod(
-            "/*[contains(.,fn:type('Unity.Multiplayer.Tools.NetworkSimulator.Runtime.NetworkSimulator'))][0]//fn:component('Unity.BossRoom.Utils.NetworkSimulatorUIMediator')",
-            "Hide");
+        var startViewerPositionOfProtagonist = BossRoomNavigators.GetRemotePlayerWorldPosition(viewerApi);
         
-        // P1 selects the first character seat
-        api.ClickObject(MouseButtons.LEFT,
-            "//Test_CharSelectSeat[0]//*[contains(.,fn:type('UnityEngine.UI.Button'))]", 1);
-        api.WaitForEmptyInput();
+        BossRoomNavigators.MoveLocalPlayer(protagApi);
         
-        // Wait for P1's selection to replicate and appear in P2's UI
-        apiP2.WaitForObject("//*[@name='ActiveBkgnd' and ../@name='AnimationContainer' and @activeInHierarchy='true']", 15);
-
-        // Assert P2 sees exactly one highlighted seat (P1's selection)
-        var selectedOnClient = apiP2.GetObjectFieldValue<int>(
-            "//*[@name='ActiveBkgnd' and ../@name='AnimationContainer' and @activeInHierarchy='true']/fn:count()");
-        Assert.That(selectedOnClient, Is.EqualTo(1));
-    }
-
-    [Test, Order(020)]
-    public void T020_GivenBothInLobby_WhenClientSelectsCharacter_BothSelectionsVisibleOnHost()
-    {
-        // P2 selects the second character seat
-        apiP2.ClickObject(MouseButtons.LEFT,
-            "//*[@name='CharacterSelectCanvas']//*[@name='PlayerSeats']//*[@name='PlayerSeat (1)']//*[@name='AnimationContainer']//*[@name='ClickInteract']", 30);
-        apiP2.WaitForEmptyInput();
-        // Give P2's selection time to replicate to P1
-        api.Wait(500);
-
-        // Assert P1 now sees two highlighted seats (P1's and P2's selections)
-        var selectedOnHost = api.GetObjectFieldValue<int>(
-            "//*[@name='ActiveBkgnd' and ../@name='AnimationContainer' and @activeInHierarchy='true']/fn:count()");
-        Assert.That(selectedOnHost, Is.EqualTo(2));
-    }
-
-    [Test, Order(030)]
-    public void T030_GivenBothInLobby_WhenBothReadyUp_EntersBossRoomWithNoErrors()
-    {
-        // Both players ready up via the ClientCharSelectState component
-        api.CallMethod("/*/fn:component('Unity.BossRoom.Gameplay.GameState.ClientCharSelectState')", "OnPlayerClickedReady");
-        apiP2.CallMethod("/*/fn:component('Unity.BossRoom.Gameplay.GameState.ClientCharSelectState')", "OnPlayerClickedReady");
-
-        // Wait for both players to transition into the BossRoom gameplay scene
-        while (api.GetSceneName() != "BossRoom")
-            api.Wait(500);
-        while (apiP2.GetSceneName() != "BossRoom")
-            apiP2.Wait(500);
-
-        // Wait for both loading screens to fade out
-        api.WaitForObjectValue("/*[@name='LoadingScreen']/fn:component('UnityEngine.CanvasGroup')", "alpha", 0f);
-        apiP2.WaitForObjectValue("/*[@name='LoadingScreen']/fn:component('UnityEngine.CanvasGroup')", "alpha", 0f);
-
-        // Dismiss the cheats and how-to-play popups on both clients
-        // CallMethod used instead of SetObjectFieldValue("active") — obsolete property, stripped in standalone
-        api.CallMethod("//*[@name='CheatsPopupPanel']", "SetActive", [false]);
-        api.CallMethod("//*[@name='HowToPlayPopupPanel']", "SetActive", [false]);
-        apiP2.CallMethod("//*[@name='CheatsPopupPanel']", "SetActive", [false]);
-        apiP2.CallMethod("//*[@name='HowToPlayPopupPanel']", "SetActive", [false]);
-
-        api.Wait(1000);
-
-        // Assert no errors were logged on P1 during the transition
-        Console.WriteLine(string.Join("\n", outputs));
-        Assert.That(isConsoleError, Is.False);
-    }
-
-    [Test, Order(040)]
-    public void T040_GivenBothInGame_WhenHostMoves_MovementReflectedOnClient()
-    {
-        // Get P1's player screen position for clicking, and record start position as seen by P2
-        var hostScreenPos = api.GetObjectPosition("/*[@name='PlayerAvatar0']", CoordinateConversion.WorldToScreenPoint);
-        var hostStartPos = apiP2.GetObjectPosition("/*[@name='PlayerAvatar0']");
-
-        // Click to the right of P1's character to trigger movement
-        var toTheRight = new Vector2(hostScreenPos.x + 200, hostScreenPos.y);
-        api.Click(toTheRight, MouseButtons.LEFT, 10);
-
-        // Wait for movement to occur and replicate to P2
-        api.Wait(1000);
-
-        // Read P1's new position as seen by P2 and assert it has moved
-        var hostEndPos = apiP2.GetObjectPosition("/*[@name='PlayerAvatar0']");
-        var distanceTravelled = Vector3.Distance(hostEndPos, hostStartPos);
-
-        Assert.That(distanceTravelled, Is.GreaterThan(0.5f));
-    }
-
-    private void OnUnityLog(object? sender, UnityLogEventEventArgs args)
-    {
-        isConsoleError |= args.type is LogType.Error or LogType.Exception or LogType.Assert;
-        outputs.Add($"[{args.type}] {args.condition}");
+        var endViewerPositionOfProtagonist = BossRoomNavigators.GetRemotePlayerWorldPosition(viewerApi);
+        
+        var playerMovement = Vector3.Distance(startViewerPositionOfProtagonist, endViewerPositionOfProtagonist);
+        Console.WriteLine(endViewerPositionOfProtagonist);
+        Assert.That(playerMovement, Is.GreaterThan(0.25f));
     }
 }
